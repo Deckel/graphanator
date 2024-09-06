1918f21c8b90e903b54d39fbb9f9f03041bc31c9
deckel <deckel.delange@gmail.com>
2024-09-06 10:53:32 +0200

    Added FileManager class

---
+++ file_manager.py
@@ -0,0 +1,15 @@
+import os
+
+class FileManager:
+    def __init__(self) -> None:
+        self.directory = f"{os.getcwd()}/data"
+        self.files = self.get_sorted_files()
+        self.context = self.get_latest_file()
+
+    def get_sorted_files(self):
+        files = [(file, os.path.getctime(os.path.join(self.directory, file))) for file in os.listdir(self.directory)]
+        sorted_files = sorted(files, key=lambda x: x[1], reverse=True)
+        return sorted_files
+    
+    def get_latest_file(self):
+        return self.get_sorted_files()[0][0]
