9878191a0cd91fcfa546cf4383a6bf2ea171dc22
deckel <deckel.delange@gmail.com>
2024-09-08 13:58:30 +0100

    Updated context

--- context_processor.py
+++ context_processor.py
@@ -19,10 +19,13 @@ class ContextProcessor:
                 For you first message, give me a brief description on what this csv represents.
                 Then suggest 3 graphs I could make to understand the dataset.
 
-                Every subsequent reply, always give one and only one peice of Python code 
-                that makes a variable called fig using matplotlib ensure this code is
-                wrapped within ``` ``` so I can execute the code to make a graph.
-                after which you will give me a brief description about the graph as well as 3 suggestions of other graphs I can make.
+                Every subsequent reply: 
+                1. Always give one and only one peice of Python code 
+                2. Assume you are given a variable df that is a DataFrame of the data
+                3. Always use matplitlib
+                4. Always create a variable fig for the graph
+                5. Wrap code within ``` ``` 
 
+                After which you will give me a brief description about the graph as well as 3 suggestions of other graphs I can make.
             """
         }
